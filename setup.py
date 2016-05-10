501b630db51c86f40d2a2f44ac46a3f8720a7b9d
Bob Colner <bobcolner@AirBob.local>
2016-05-09 17:50:15 -0700

    updated after pypi push

--- setup.py
+++ setup.py
@@ -18,19 +18,15 @@ setup(
         #   4 - Beta
         #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
-
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Database Tools',
-
-        # Pick your license as you wish (should match "license" above)
+        'Topic :: Database :: Front-Ends    ',
+        # Pick your license as you wish (should match 'license' above)
         'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
+        # Specify the Python versions you support here.
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
-    install_requires=["psycopg2", "jsonpickle", "tqdm"],
+    install_requires=['psycopg2', 'jsonpickle', 'tqdm']
 )
